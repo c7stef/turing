9a9e6340dc547ac4fcc1d3b8ff4c711f3a3d97e7
Cosmin Popa <cosmin7popa@gmail.com>
2024-10-23 23:42:07 +0300

    Add optimization flag to x86 toolchain file
    Signed-off-by: Cosmin Popa <cosmin7popa@gmail.com>

--- x86_64-linux-clang.cmake
+++ x86_64-linux-clang.cmake
@@ -2,6 +2,6 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 set(CMAKE_CXX_COMPILER clang++-18)
-set(CMAKE_CXX_FLAGS "-stdlib=libc++ -Wall -Wextra -pedantic")
+set(CMAKE_CXX_FLAGS "-stdlib=libc++ -Wall -Wextra -pedantic -O3")
 set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++")
 set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)
